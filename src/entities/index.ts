2de9dc9c1c4fb645e55c7015ccdafef97a206f6f
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-11-10 22:29:59 +0900

    refactor: entity index

---
+++ src/entities/index.ts
@@ -0,0 +1,4 @@
+export { MenuEntity } from './menu.entity';
+export { OrderEntity } from './order.entity';
+export { OrderMenuEntity } from './orderMenu.entity';
+export { UserEntity } from './users.entity';
--- src/entities/orderMenu.entity.ts
+++ src/entities/orderMenu.entity.ts
@@ -1,8 +1,9 @@
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { MenuEntity } from './menu.entity';
+import { OrderEntity } from './order.entity';
 
 @Entity()
-export class OrderEntity {
+export class OrderMenuEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
